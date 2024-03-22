d571bfe857292bc163a1ec737f91eccd9246b457
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-22 09:26:09 +0300

    feat: add functions and variables to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = '';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'disliked customer';
+}
